b19aab7153c8425dcfd73334389d9b0b7f779181
Chris Gaye <cjgazelle@gmail.com>
2023-10-25 23:55:43 -0400

    added some questions to index.js

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 // TODO: Include packages needed for this application
+import generateMarkdown from "./utils/generateMarkdown";
+import fs from "fs";
+import inquirer from "inquirer";
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the name of your project.",
+    },
+    {
+        type:"input",
+        name:"description",
+        message:"Give a description of your project please!",
+    },
+    {
+        type:"input",
+        name:"installation",
+        message:"What steps are required to install your project.",
+    },
+    {
+        type:"input",
+        name:"usage",
+        message:"Provide instructions and examples for the usage of your project.",
+    },
+    {
+        type:"input",
+        name:"credits",
+        message:"List collaborators, tutorials used, and third-party assets, if there are any.",
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
